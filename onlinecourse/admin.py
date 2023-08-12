d0ffa213f5838121c4f235008de3fae151edf16e
softwareguy <testandlearn0920@gmail.com>
2023-08-12 13:23:23 -0400

    second commit and error

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
 
 
 class LessonInline(admin.StackedInline):
@@ -21,8 +28,17 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
 
 # <HINT> Register Question and Choice models here
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
